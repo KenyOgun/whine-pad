5eaf731d26a1af34f0f5f71a4831ff0cf58a9ddd
KenyOgun <holleraturboi@yahoo.ca>
2022-10-08 07:58:26 -0400

    added folders

---
+++ src/components/Excel.js
@@ -0,0 +1,11 @@
+import { useState, useReducer } from "react";
+import PropTypes from "prop-types";
+import clone from "../modules/clone";
+
+// do the work
+function Excel({ headers, initialData }) {
+  // same as before
+}
+
+// export
+export default Excel;
